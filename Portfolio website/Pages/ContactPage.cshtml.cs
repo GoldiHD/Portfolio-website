15676e9d8cabfd14ecd7b3d0eb7e89c373c47d2a
Gold(Lars H M) <lars.h.m@hotmail.com>
2020-06-10 14:27:10 +0200

    Connected database. Connected to Azure for publishing. Navbar Created but needs a later revamp. Landing page is underconstruction and still need both data and setup. Possible option for changing between danish and english

---
+++ Portfolio website/Pages/ContactPage.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_website.Pages
+{
+    public class ContactPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
